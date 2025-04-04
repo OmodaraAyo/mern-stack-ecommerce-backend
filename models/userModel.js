b7d6aaef67b3a1eea9fa37eebc114de63d95084a
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-04-04 17:25:40 +0100

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: String,
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture : String
+},{
+    timestamps: true
+})
+
+const userModel = mongoose.model('user', userSchema)
+
+module.exports = userModel
