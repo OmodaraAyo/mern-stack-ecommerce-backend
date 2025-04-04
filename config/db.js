6ee70f08d8eff798eda753fae96cf51320e90b47
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-04-04 17:25:40 +0100

    first commit

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+async function connectDB(){
+    try{
+        mongoose.connect(process.env.MONGODB_URI)
+        console.log('MongoDB connected')
+    }catch(err){
+        console.log(err.message)
+    }
+} 
+
+module.exports = connectDB
+// const mongoose = require('mongoose')
---
+++ controller/userSignUp.js
@@ -0,0 +1,57 @@
+const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+async function userSignUpController(req, res) {
+    // console.log("req.body", req.body)
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await userModel.findOne({ email: email})
+    if(user){
+        throw new Error("Email already exists");
+    }
+    
+    if (!name) {
+      throw new Error("Please provide name");
+    }
+    if (!email) {
+      throw new Error("Please provide email");
+    }
+    if (!password) {
+      throw new Error("Please provide password");
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+    if (!hashPassword){
+        throw new Error("Something went wrong");
+    }
+
+    const payload = {
+        ...req.body,
+        role : "user",
+        password:  hashPassword,
+    }
+
+    const userData = new userModel(payload);
+    const saveUser = await userData.save();
+
+    res.status(201).json({
+        data : saveUser,
+        success : true,
+        error : false,
+        message: "Account created successfully",
+    })
+
+  } catch (error) {
+    res.json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+module.exports = userSignUpController;
+// const userModel = require("../models/userModel");
