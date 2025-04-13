fc3d9b190fd5435796d950a5dd576fb7cd785dad
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-04-13 02:39:06 +0100

    admin

---
+++ controller/allUsers.js
@@ -0,0 +1,24 @@
+const userModel = require("../models/userModel");
+
+async function allUsers(req, res) {
+    try {
+        const allUsers = await userModel.find()
+
+         res.json({
+            message: "All users",
+            data : allUsers,
+            error: false,
+            success: true,
+         })
+        
+    } catch (error) {
+        res.status(500).json({ 
+            message: error.message || error || "Internal Server Error",
+            error : true,
+            success : false 
+        });
+        
+    }
+}
+
+module.exports = allUsers
