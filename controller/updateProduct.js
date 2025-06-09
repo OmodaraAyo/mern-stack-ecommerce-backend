9360e1a70029e8a77f90cc4d860a08a8cfb21e1f
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-06-09 01:17:41 +0100

    update product features added

--- controller/updateProduct.js
+++ controller/updateProduct.js
@@ -1,15 +1,20 @@
+const uploadProductPermission = require("../helpers/permission");
+const productModel = require("../models/productModel");
+
 async function updateProductController(req, res) {
     try {
 
-        if(!uploadProductPermission(sessionUserId)) {
+        console.log("Update product request body from the backend: ", req);
+        if(!uploadProductPermission(req.userId)) {
             throw new Error("Permission denied");
         }
 
-        const { _id, ...reqBody } = req.body;
+        const { _id, ...resBody } = req.body;
 
         const updateProduct = await productModel.findByIdAndUpdate(
             _id, 
-            reqBody,
+            resBody,
+            { new: true }
         );
         if (!updateProduct) {
             throw new Error("Product not found");
