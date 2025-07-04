c85e50ea0e0b86dc668f94a0b4020d88b962f756
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-07-04 22:32:13 +0100

    bcryptjs installed for deployment...

--- controller/user/userSignUp.js
+++ controller/user/userSignUp.js
@@ -1,5 +1,5 @@
 const userModel = require("../../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 async function userSignUpController(req, res) {
